ea20ca25e57d1b60b1e91fb76497b525875ad8b8
zulkifalkhan <zulkifalkhan436@gmail.com>
2024-06-04 18:14:47 +0500

    mongodb connected

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,15 @@
 import express from "express";
+
+import dotenv from "dotenv";
+import connectDB from "./framework/initDatabase";
+
+dotenv.config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
+
+// Connect to database
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
